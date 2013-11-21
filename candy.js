0df93ee2d82a4c753cae971c0a401aee6adf0173
nievie <nievie@gmail.com>
2013-11-21 15:21:41 -0700

    making a mess with no appologies!

--- candy.js
+++ candy.js
@@ -17,7 +17,7 @@ candy_random = function(){
 	return candie;
 }
 
-makeCandy = function(){
+placeRandomCandy = function(){
 	var candy_span = document.createElement('button');
 	var new_candy = candy_random();
 	candy_span.className = new_candy.color;
@@ -25,47 +25,71 @@ makeCandy = function(){
 	document.body.appendChild(candy_span);
 }
 
-removeCandy = function(){
-
-}
 
 // this is where the grid will be built and populated
 
-
-
-
 makeRow = function(){
-var row = [];
-	while (row.length < 9){
-		row.push(candy_random());
-	}
-return row;
+	var row = [];
+		while (row.length < 9){
+			row.push(candy_random());
+		}
+	return row;
 }
 
-var row = function(){
-	var r = makeRow();
-	return r;
-}
-
-
+// var row = function(){
+// 	var r = makeRow();
+// 	return r;
+// }
 
 makeGrid = function(){
-	var one = new row();
-	var two = new row();
-	var three = new row();
-	var four = new row();
-	var five = new row();
-	var six = new row();
-	var seven = new row();
-	var eight = new row();
-	var nine = new row();
-	var grid = [one, two, three, four, five, six, seven, eight, nine];
+	var grid = []
+	for (var i = 0; i < 9; i++) {
+		grid.push(makeRow());
+	};
 	return grid;
 }
 
-	for (var i = 0; i < 9; i++) {
-		makeRow();
-	};
+var starterGrid = makeGrid();
+
+var land = document.getElementsByClassName('land');
+var candyLand = document.createElement('div');
+
+var convert_candy = function(){
+	var html_candy = {};
+	var html_row_one = starterGrid[0];
+	var html_row_two = starterGrid[1];
+	var html_row_three = starterGrid[2];
+	var html_row_four = starterGrid[3];
+	var html_row_five = starterGrid[4];
+	var html_row_six = starterGrid[5];
+	var html_row_seven = starterGrid[6];
+	var html_row_eight = starterGrid[7];
+	var html_row_nine = starterGrid[8];
+	var html_column_one = html_row_one[0]
+	html_column_one.color
+
+
+} 
+
+convertGridToHtml = function(){
+	var candy_span = document.createElement('button');
+
+	var new_candy = 'the product of each iteration of the loop through starterGrid'; 
+	candy_span.className = new_candy.color;
+	candy_span.style.color = new_candy.color;
+	document.body.appendChild(candy_span);
+}
+
+function logArrayElements(element, index, array) {
+    console.log("a[" + index + "] = " + element);
+}
+
+[2, 5, 9].forEach(logArrayElements);
+
+function getIndex(element, index, array) {
+	console.log(index + element);
+}
+
 
 // helper with starting spot and a direction, tells you how far you can go 
 
